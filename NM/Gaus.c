487e4fa980ee4d5be3304762be195b6921d9c31d
Gleb <gleb-skiba@mail.ru>
2017-11-26 18:48:03 +0300

    Make new fect

--- NM/Gaus.c
+++ NM/Gaus.c
@@ -21,7 +21,7 @@ typedef struct matrix{
 } matrix_t;
 typedef matrix_t *matrix_ptr;
 ////////////////////////////////////////////////////////////////////////
-int32_t __row_new(row_ptr *row_new, int32_t m) {
+int32_t row_new(row_ptr *row_new, int32_t m) {
     if (m >= ROW_MAX_LEN) {
         return 1;
     }
@@ -32,7 +32,7 @@ int32_t __row_new(row_ptr *row_new, int32_t m) {
     (*row_new)->m = m;
     return 0;
 }
-int32_t __matrix_new(matrix_ptr *mat_new, int32_t n, int32_t m){
+int32_t matrix_new(matrix_ptr *mat_new, int32_t n, int32_t m){
     if ((n >= ROW_MAX_NUM) || (m >= ROW_MAX_LEN)) {
         return 1;
     }
@@ -52,11 +52,14 @@ int32_t __matrix_new(matrix_ptr *mat_new, int32_t n, int32_t m){
         }
     return 0;
 }
-void __matrix_del(matrix_ptr matr) {
+void matrix_del(matrix_ptr matr) {
+    if (!matr) {
+        return;
+    }
     for (int32_t i = 0; i < matr->n; i++) {
         free(matr->row[i]);
     }
-    free(colum);
+    free(matr->colum);
     free(matr);
 }
 /////////////////////////////////////////////////////////////////////////
@@ -68,42 +71,61 @@ int32_t el_func_gen_b(int32_t i) {
     int32_t mid = i;
     return mid;
 }
-int32_t matrix_gen(matrix_ptr matr, int32_t n, int32_t m){
-    if (__matrix_new(&matr, n, m)) {
-        __matrix_del(matr);
-        return 1;
-    }
-    for (int32_t i = 0; i < n; i++) {
-        for (int32_t j = 0; j < m; j++) {
-            matr->row[i]->el[m] = el_func_gen(i, j);
+int32_t matrix_gen(matrix_ptr matr){
+    for (int32_t i = 0; i < matr->n; i++) {
+        for (int32_t j = 0; j < matr->m; j++) {
+            matr->row[i]->el[j] = el_func_gen(i, j);
         }
         matr->colum->el[i] = el_func_gen_b(i);
     }
     return 0;
 }
-int32_t matrix_input(matrix_ptr matr, int32_t n, int32_t m) {
-    if (__matrix_new(&matr, n, m)) {
-        __matrix_del(matr);
-        return 1;
-    }
-    for (int32_t i = 0; i < n; i++) {
-        for (int32_t j = 0; j < m; j++) {
+int32_t matrix_input(matrix_ptr matr) {
+    for (int32_t i = 0; i < matr->n; i++) {
+        for (int32_t j = 0; j < matr->m; j++) {
             scanf("%d", &matr->row[i]->el[j]);
         }
         scanf("%d", &matr->colum->el[i]);
     }
     return 0;
 }
-void matrix_output(matrix_ptr matr, int32_t n, int32_t m) {
-    for (int32_t i = 0; i < n; i++) {
-        for (int32_t j = 0; j <m; j++) {
+void matrix_output(matrix_ptr matr) {
+    if (!matr) {
+        return;
+    }
+    printf("print \n");
+    for (int32_t i = 0; i < matr->n; i++) {
+        for (int32_t j = 0; j < matr->m; j++) {
             printf("%d ", matr->row[i]->el[j]);
         }
         printf("%d\n",matr->colum->el[i]);
     }
 }
 /////////////////////////////////////////////////////////////////////////
+void int_swap(int32_t *first, int32_t *second) {
+    int32_t mid = *first;
+    *first = *second;
+    *second = mid;
+}
+int32_t row_swap(row_ptr first, row_ptr second) {
+    if (first->m != second->m) {
+        return 1;
+    }
+    for (int32_t i = 0; i < first->m; i++) {
+        int_swap(&first->el[i],&second->el[i]);
+    }
+    return 0;
+}
+/////////////////////////////////////////////////////////////////////////
 int main(void) {
-    printf("%d\n",el_func_gen(1, 2));
+    
+    matrix_ptr matr = NULL;
+    if (matrix_new(&matr,5,5)) {
+        matrix_del(matr);
+        return 0;
+    }
+    printf("%d\n",matrix_gen(matr));
+    matrix_output(matr);
+    matrix_del(matr);
     return 0;
 }
